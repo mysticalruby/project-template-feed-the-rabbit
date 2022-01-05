776f06cb42809f7a301a146c526cd699e389f3c3
mysticalruby <90880156+mysticalruby@users.noreply.github.com>
2022-01-04 21:25:52 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -43,7 +43,7 @@ function draw() {
 
 // var select_sprites = Math.round(1,3);
 
-// var select_sprites = Math.round(random(1,3));
+ var select_sprites = Math.round(random(1,3));
 
   
   // if (frameCount % 50 == 0) {
@@ -74,15 +74,15 @@ function draw() {
   //   }
   // }
 
-  // if (frameCount % 80 = 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
-  // }
+   if (frameCount % 80 === 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }else {
+       createRed();
+     }
+   }
 
 
 
